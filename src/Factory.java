56220394b2dc2c65d962b94b8535e2e3ee0d3cf2
pycaballerom <36699772+pycaballerom@users.noreply.github.com>
2019-07-16 20:02:14 -0500

    Add files via upload

---
+++ src/Factory.java
@@ -0,0 +1,14 @@
+class Factory {
+    private Gazillion[] pool;
+
+    public Factory(int maxRows) {
+        pool = new Gazillion[maxRows];
+    }
+
+    public Gazillion getFlyweight(int row) {
+        if (pool[row] == null) {
+            pool[row] = new Gazillion(row);
+        }
+        return pool[row];
+    }
+}
---
+++ src/Gazilion.java
@@ -0,0 +1,12 @@
+class Gazillion {
+    private int row;
+
+    public Gazillion(int row) {
+        this.row = row;
+        System.out.println("ctor: " + this.row);
+    }
+
+    void report(int col) {
+        System.out.print(" " + row + col);
+    }
+}
